52e98d9cc92d4e2122f7abfe7ed895fe01f7ff05
PavelDavydenko <3dsworld@mail.ru>
2022-02-05 17:04:09 +0300

    release_1.0

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] resault = new int[N];
+            int count = 1;
+            bool flag;
+            Console.WriteLine("--------------------------------------------");
+            if (N < 2) {
+                Console.WriteLine("Нет простых чисел, которые меньше N = " + N);
+                return;
+            }
+            Console.WriteLine("Все простые числа, которые меньше N = " + N);
+            Console.WriteLine("2");
+            for (int i = 3;i <= N;i += 2){
+                flag = true;
+                for (int j = 3;j <= Math.Sqrt(i);j += 2)
+                {
+                    if (i % j == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+            Console.WriteLine("Количество: " + count);
+        }
+    }
+}
